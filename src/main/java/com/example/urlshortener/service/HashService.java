3072423a908662024887a2d52f498e4a07516ff7
uchitsa <uchitsa@ya.ru>
2022-02-04 02:58:08 +0300

    add HashService, update pom.xml

---
+++ src/main/java/com/example/urlshortener/service/HashService.java
@@ -0,0 +1,13 @@
+package com.example.urlshortener.service;
+
+import org.apache.commons.text.RandomStringGenerator;
+
+public class HashService {
+
+    private RandomStringGenerator randomStringGenerator;
+
+    //TODO: complete HashService
+
+
+}
+
