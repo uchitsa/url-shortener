e44e00182c4cef5276cc2979096e82d41d380648
uchitsa <uchitsa@ya.ru>
2022-02-05 01:57:58 +0300

    update HashService

--- src/main/java/com/example/urlshortener/service/HashService.java
+++ src/main/java/com/example/urlshortener/service/HashService.java
@@ -4,10 +4,29 @@ import org.apache.commons.text.RandomStringGenerator;
 
 public class HashService {
 
-    private RandomStringGenerator randomStringGenerator;
+    private final RandomStringGenerator randomStringGenerator;
 
-    //TODO: complete HashService
+    public HashService() {
+        this.randomStringGenerator = new RandomStringGenerator
+                .Builder()
+                .filteredBy(HashService::isAlphaNumeric)
+                .build();
+    }
 
+    private static boolean isAlphaNumeric(int c) {
+        return isAlpha(c) || isNumeric(c);
+    }
 
+    private static boolean isNumeric(int c) {
+        return '0' <= c && c <= '9';
+    }
+
+    private static boolean isAlpha(int c) {
+        return ('a' <= c && c <= 'z') || ('A' <= c && c <= 'Z');
+    }
+
+    public String generateHash(int length) {
+        return randomStringGenerator.generate(length);
+    }
 }
 
